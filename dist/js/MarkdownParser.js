afee92050a85a859b0b1a074bd707d5e89d1c485
Jay Bien-aimé <jay.bienaime@gmail.com>
2022-01-01 19:16:53 -0500

    plus tagtypes and class to map tagtypes

--- dist/js/MarkdownParser.js
+++ dist/js/MarkdownParser.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HtmlHandler = void 0;
 class HtmlHandler {
     TextChangeHandler(id, output) {
         const markdown = document.getElementById(id);
@@ -13,5 +15,22 @@ class HtmlHandler {
                 }
             };
         }
+    }
+}
+exports.HtmlHandler = HtmlHandler;
+var TagType;
+(function (TagType) {
+    TagType[TagType["Header1"] = 0] = "Header1";
+    TagType[TagType["Header2"] = 1] = "Header2";
+    TagType[TagType["Header3"] = 2] = "Header3";
+    TagType[TagType["Paragraph"] = 3] = "Paragraph";
+})(TagType || (TagType = {}));
+class TagToHtml {
+    constructor() {
+        this.tagType = new Map();
+        this.tagType.set(TagType.Header1, "h1");
+        this.tagType.set(TagType.Header2, "h2");
+        this.tagType.set(TagType.Header3, "h3");
+        this.tagType.set(TagType.Paragraph, "p");
     }
 }
--- src/MarkdownParser.ts
+++ src/MarkdownParser.ts
@@ -19,3 +19,24 @@ class HtmlHandler {
     }
 }
 
+enum TagType {
+    Header1,
+    Header2,
+    Header3,
+    Paragraph
+}
+
+class TagToHtml{
+    private readonly tagType: Map< TagType, string > = new Map< TagType, string >();
+
+    constructor() {
+        this.tagType.set(TagType.Header1, "h1");
+        this.tagType.set(TagType.Header2, "h2");
+        this.tagType.set(TagType.Header3, "h3");
+        this.tagType.set(TagType.Paragraph, "p");
+    }
+
+}
+
+
+export { HtmlHandler }
